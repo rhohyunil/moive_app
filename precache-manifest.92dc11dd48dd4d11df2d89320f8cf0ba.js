cb37d65c3e77ea5fe0e07b5db0269dc54d3447e5
盧賢一 <rhohyunil@gmail.com>
2020-09-21 08:38:14 +0900

    Updates

---
+++ precache-manifest.92dc11dd48dd4d11df2d89320f8cf0ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9195b83c12b4382945034603723bf62",
+    "url": "/moive_app/index.html"
+  },
+  {
+    "revision": "773288d2302bb2228c7a",
+    "url": "/moive_app/static/css/main.1a6dc005.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/moive_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moive_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "773288d2302bb2228c7a",
+    "url": "/moive_app/static/js/main.5db1ac63.chunk.js"
+  },
+  {
+    "revision": "86e70080cddc6766b85a",
+    "url": "/moive_app/static/js/runtime-main.2ec614c7.js"
+  }
+]);
